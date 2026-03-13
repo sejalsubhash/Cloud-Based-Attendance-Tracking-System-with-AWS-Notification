decd35e5900cfb9bc72b90b2c249bdea11988fe0
Sejal Pawar <sejal.pawar@acc.ltd>
2026-03-13 17:06:41 +0530

    fix render deployment

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,14 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-// const punchRoutes = require("./routes/punch");
-
 const app = express();
 
-/* Middleware */
 app.use(cors());
 app.use(express.json());
 
-/* API Routes */
-app.use("/api", punchRoutes);
+app.get("/", (req, res) => {
+  res.send("Punch API running successfully");
+});
 
 /* Serve Frontend Build */
 const frontendPath = path.join(__dirname, "../frontend/build");
@@ -23,7 +21,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-/* Server Port */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
